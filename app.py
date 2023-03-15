28b9a5fdd99234993da97637e4f5dcd721cf192a
luna <symphorienlipandz@yahoo.com>
2023-03-15 14:37:50 +0000

    Added some data and flask function

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
 #Importing library 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 app = Flask(__name__)
+JOBS = [
+  {
+    'id':1,
+    'title':'Data Analyst',
+    'location':'Tunis,Tunisa',
+    'salary':'Dt. 1000'
+  },
+  {
+    'id':2,
+    'title':'Data Scientist',
+    'location':'Tunis,Tunisa',
+    'salary':'Dt. 1100'
+  },
+  {
+    'id':3,
+    'title':'Cloud Architect',
+    'location':'Remote',
+  },
+  {
+    'id':4,
+    'title':'IoT Engineer',
+    'location':'Nashville, Tennesses, USA',
+    'salary':'$ 3000'
+  }
+]
 @app.route('/')
-
 def hello_word():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name="EcoLink")
+
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
