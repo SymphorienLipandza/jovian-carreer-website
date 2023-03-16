7b11bf725c2cf39ebe4436bc29f257d98bc50073
luna <symphorienlipandz@yahoo.com>
2023-03-16 03:34:45 +0000

    Dynamic page rendering for each jobs

--- database.py
+++ database.py
@@ -20,5 +20,16 @@ def load_jobs_from_db():
       jobs.append(row._asdict())
     return jobs
 
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text(f"select *from jobs where id= {id}")
+  )
+    rows= result.all()
+    if len(rows)==0:
+      return None
+    else:
+      return rows[0]._asdict()
+      
 print(sqlalchemy.__version__)
 
