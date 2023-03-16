688076d5ef35b905ddef17968f359e14030a910b
luna <symphorienlipandz@yahoo.com>
2023-03-16 07:55:59 +0000

    Finally, we are done with the career site with  flask by adding  dynamically application to the cloud database

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #Importing library 
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db, load_job_from_db
+from flask import Flask, render_template, jsonify, request
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 
 app = Flask(__name__)
@@ -14,13 +14,27 @@ def hello_word():
 def list_jobs():
   jobs=load_jobs_from_db()
   return jsonify(jobs)
+
+@app.route('/api/job/<id>')
+def show_job_json(id):
+  job= load_job_from_db(id)
+  return jsonify(job)
+
 @app.route("/job/<id>")
 def show_job(id):
   job= load_job_from_db(id)
   if not job:
     return "Not Found", 404
   return render_template('jobpage.html', job=job)
+  
 
+@app.route('/job/<id>/applay', methods=['post'])
+def applay_to_job(id):
+  data = request.form
+  job=load_job_from_db(id)
+  add_application_to_db(id, data)
+  return render_template('applicationsubmitted.html',application=data, job=job)
+  
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
 
